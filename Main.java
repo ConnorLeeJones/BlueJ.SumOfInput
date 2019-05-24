e466041562aa911e00337217908a6c0aa69e99be
Connor Jones <ConnorLeeJones@gmail.com>
2019-05-23 21:48:41 -0400

    completed lab

--- Main.java
+++ Main.java
@@ -1,9 +1,20 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Please enter a number:");
+        int n = scnr.nextInt();
+        int sum = 0;
+        
+        for (int i = 1; i <= n; i++){
+            sum += i;        
+        }
+        System.out.println("The sum of 1 to " + n + " is: " + sum + ".");
     }
 }
